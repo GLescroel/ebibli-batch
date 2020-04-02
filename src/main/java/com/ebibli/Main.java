5db45bc2929bcaba26f2da9dd28fb4e1371d9f62
GREGORY LESCROEL <gregory.lescroel@axa.fr>
2020-04-02 12:19:56 +0200

    Ajout du cron pour le Schedule quotidien

--- src/main/java/com/ebibli/Main.java
+++ src/main/java/com/ebibli/Main.java
@@ -3,24 +3,26 @@ package com.ebibli;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        new Main().runAsJavaApplication(args);
+        runMyApp();
     }
 
-    private void runAsJavaApplication(String[] args) {
-        LOGGER.info("*********** Démarrage du batch ***********");
-        SpringApplicationBuilder application = new SpringApplicationBuilder();
-        application.sources(Main.class);
-        application.run(args);
+    @Scheduled(cron = "${batch.cron.value}")
+    private static void runMyApp() {
+        final ConfigurableApplicationContext context = SpringApplication.run(Main.class);
+        SpringApplication.exit(context);
         LOGGER.info("*********** Batch terminé ***********");
-        System.exit(0);
     }
 }
